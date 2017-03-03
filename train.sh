b6796e2f21ab5102cc9300270a1d3dbf4068de2f
Sudeep Fadadu <sudeep@cmu.edu>
2017-03-03 00:31:34 -0500

    commiting local changes

--- train.sh
+++ train.sh
@@ -1,9 +1,9 @@
 nice -n 10 python train.py \
 --structure_string=1-2-64 \
---data_split_string_train=S1-S0-S5-S6-S7-S8 \
+--data_split_string_train=S1 \
 --data_split_string_test=S9 \
 --batch_size=2 \
 --joint_prob_max=10 \
 --sigma=1 \
---gpu_string=0 \
+--gpu_string=0-1-2-3 \
 --learning_rate=5e-3
